edfb314307b2a11dda0a5dd3bca25ff2dcbd17f3
Ahmad Hasan <r.ahmad.hasan.r@gmail.com>
2023-01-30 13:54:57 +0600

    sell section edited

--- src/Pages/Home/Sell/Sell.js
+++ src/Pages/Home/Sell/Sell.js
@@ -4,17 +4,16 @@ import { Link } from 'react-router-dom';
 const Sell = () => {
     return (
         <div className='my-10'>
-            <div className="hero rounded min-h-screen mx-5 max-w-[1200px] bg-gradient-to-b from-[#414141] to-[#000000] my-10 lg:mx-auto">
-            <div className="hero-content flex-col lg:flex-row lg:justify-between">
-                <div>
-                <img src="https://www.trustedreviews.com/wp-content/uploads/sites/54/2022/03/best-phone-2022-920x613.jpg" className="max-w-sm rounded-lg shadow-2xl w-full hover:scale-105" alt='' />
+                <div className='max-w-[1200px] mx-auto md:flex md:flex-row bg-gradient-to-b from-[#414141] to-[#000000] lg:px-10 lg:py-10 sm:py-10 rounded'>
+                <div className='md:w-1/2 px-10 py-10 md:py-0'>
+                    <img className='w-full hover:scale-105 rounded-md' src="https://www.trustedreviews.com/wp-content/uploads/sites/54/2022/03/best-phone-2022-920x613.jpg" alt='banner'></img>
                 </div>
-                <div>
-                <h1 className="text-5xl font-bold text-white">Sell Products!</h1>
-                <p className="py-6 text-white">Sell your used phones and tablets that are in good condition</p>
-                <button className="btn bg-cyan-700 hover:bg-sky-900 w-32 btn-sm text-white"><Link to='/addphone'>Get Started</Link></button>
+                <div className='text-center flex flex-col justify-center md:w-1/2 md:text-left px-10 pb-10 md:py-0'>
+                <h1 className="mb-5 text-5xl text-white font-bold">Sell Products!</h1>
+                <p className="mb-5 text-white">Sell your used phones and tablets that are in good condition</p>
+                <div className='text-center md:text-left'><button className="btn bg-cyan-700 hover:bg-sky-900 w-32 btn-sm text-white"><Link to='/addphone'>Get Started</Link></button></div>
                 </div>
-            </div>
+               
             </div>
         </div>
     );
